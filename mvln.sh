9a04ac7fa587e123d5e9fa0210a3db34fdc3bf0e
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2013-01-12 07:57:38 -0800

    mvln.sh: move things to a new directory but leave behind symbolic links

---
+++ mvln.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+usage() {
+    echo "USAGE: $(basename $0) SOURCE... DIRECTORY"
+    echo "Move SOURCE(s) to DIRECTORY, leaving behind symbolic links."
+    exit 1
+}
+
+[[ $# -ge 2 ]] || usage
+DIRECTORY="${@: -1}"
+[[ -d $DIRECTORY ]] || usage
+while [[ $# -gt 1 ]]
+do
+    [[ -z $DEBUG ]] || echo "mv $1 $DIRECTORY && ln -s $(realpath $DIRECTORY)/$(basename $1) $1"
+    [[ -z $DEBUG ]] && mv $1 $DIRECTORY && ln -s $(realpath $DIRECTORY)/$(basename $1) $1
+    shift
+done
