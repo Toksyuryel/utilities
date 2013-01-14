9bf9a5c66d0dabb9ac22102401a4df51adde8c7c
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2013-01-13 20:45:45 -0800

    Be verbose, and a little bit more robust.

--- minecraft-fixer.sh
+++ minecraft-fixer.sh
@@ -5,15 +5,15 @@ die() {
 }
 
 [[ -e $HOME/.minecraft ]] || die "You don't even *have* Minecraft."
-cd $LWJGLDIR || die "Please set LWJGLDIR to the location of lwjgl on your system."
-[[ -e jar/jinput.jar ]] || die "Please set LWJGLDIR to the location of lwjgl on your system."
+[[ -d $LWJGLDIR ]] || die "Please set LWJGLDIR to the location of lwjgl on your system."
+[[ -e $LWJGLDIR/jar/jinput.jar ]] || die "Please set LWJGLDIR to the location of lwjgl on your system."
 
-cp jar/jinput.jar ~/.minecraft/bin/jinput.jar
-cp jar/lwjgl.jar ~/.minecraft/bin/lwjgl.jar
-cp jar/lwjgl_util.jar ~/.minecraft/bin/lwjgl_util.jar
-cp native/linux/libjinput-linux.so ~/.minecraft/bin/natives/libjinput-linux.so
-cp native/linux/libjinput-linux64.so ~/.minecraft/bin/natives/libjinput-linux64.so
-cp native/linux/liblwjgl.so ~/.minecraft/bin/natives/liblwjgl.so
-cp native/linux/liblwjgl64.so ~/.minecraft/bin/natives/liblwjgl64.so
-cp native/linux/libopenal.so ~/.minecraft/bin/natives/libopenal.so
-cp native/linux/libopenal64.so ~/.minecraft/bin/natives/libopenal64.so
+cp -v $LWJGLDIR/jar/jinput.jar ~/.minecraft/bin/jinput.jar
+cp -v $LWJGLDIR/jar/lwjgl.jar ~/.minecraft/bin/lwjgl.jar
+cp -v $LWJGLDIR/jar/lwjgl_util.jar ~/.minecraft/bin/lwjgl_util.jar
+cp -v $LWJGLDIR/native/linux/libjinput-linux.so ~/.minecraft/bin/natives/libjinput-linux.so
+cp -v $LWJGLDIR/native/linux/libjinput-linux64.so ~/.minecraft/bin/natives/libjinput-linux64.so
+cp -v $LWJGLDIR/native/linux/liblwjgl.so ~/.minecraft/bin/natives/liblwjgl.so
+cp -v $LWJGLDIR/native/linux/liblwjgl64.so ~/.minecraft/bin/natives/liblwjgl64.so
+cp -v $LWJGLDIR/native/linux/libopenal.so ~/.minecraft/bin/natives/libopenal.so
+cp -v $LWJGLDIR/native/linux/libopenal64.so ~/.minecraft/bin/natives/libopenal64.so
