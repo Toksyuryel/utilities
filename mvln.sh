f62a122234f460bd9bdbedbb8196a1b02436c8d8
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2013-01-13 19:42:06 -0800

    Cleanup of imported code.

--- mvln.sh
+++ mvln.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+die() {
+    echo -e $1; exit 1
+}
+
+depend() {
+    for COMMAND in $@; do
+        which $COMMAND &> /dev/null || die "FATAL ERROR: Required command '$COMMAND' is missing."
+    done
+}
+
 usage() {
     echo "USAGE: $(basename $0) SOURCE... DIRECTORY"
     echo "Move SOURCE(s) to DIRECTORY, leaving behind symbolic links."
     exit 1
 }
 
-depend_on() {
-    for command in $@; do
-        which $command &>/dev/null
-        if [[ $? -ne 0 ]]; then
-            echo "This script depends on the '$command' command, please obtain it."
-            exit 1
-        fi
-    done
-}
-
-depend_on realpath
+depend realpath
 
 [[ $# -ge 2 ]] || usage
 DIRECTORY="${@: -1}"
