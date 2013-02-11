1d50f3f4b728e842ae4513cf8080019733ab0341
Bit Shift <bitshift@bigmacintosh.net>
2013-02-11 23:36:50 +0000

    Add very hacky HTML renderer. Needs cleanup later.

--- prettify.py
+++ prettify.py
@@ -26,6 +26,188 @@ def read_weechat(infile, args):
                     " ".join(line_parts[3:]))
 
 
+@renderer_function("html", "emits HTML",
+        (('-t', '--timer', {
+            'action': 'store_true',
+            'dest': 'add_timer',
+            'help': '''Include a javascript-based timer. Incurs some significant
+                extra load time on long logs.''',
+            }),
+         ('-n', '--log-name', {
+            'action': 'store',
+            'dest': 'log_name',
+            'default': 'Prettified Log',
+            'help': '''Name to use for the log's title. If not provided,
+            "Prettified Log" will be used.''',
+            }),
+        ))
+def render_html(line_generator, args):
+    yield '''<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>''' + args.log_name + '''</title>
+    <style type="text/css">
+        body {
+            padding: 2.5em;
+        }
+        h1 {
+            margin-top: 0;
+            font-size: 1.5em;
+            text-align: center;
+        }
+        h1, .log {
+            width: 100%;
+        }
+        .username {
+            font-weight: bold;
+        }
+        .log {
+            display: table;
+        }
+        .line {
+            display: table-row;
+            opacity: 1;
+            -webkit-transition: opacity .3s ease-in;
+            -moz-transition: opacity .3s ease-in;
+            -o-transition: opacity .3s ease-in;
+            transition: opacity .3s ease-in;
+        }
+        .line.timeHidden {
+            opacity: 0;
+        }
+        .timestamp, .username, .message {
+            display: table-cell;
+            padding: 0.6em;
+        }
+        .line:nth-child(odd) {
+            background-color: #e0dcdc;
+        }
+        .line:nth-child(even) {
+            background-color: #f0ecec;
+        }
+        .timerLinks {
+            position: fixed;
+            top: 0.3em;
+            left: 0.3em;
+            background-color: #fff;
+        }
+        .timerLinks a {
+            border: none;
+            opacity: 0.4;
+            -webkit-transition: opacity .3s ease-in;
+            -moz-transition: opacity .3s ease-in;
+            -o-transition: opacity .3s ease-in;
+            transition: opacity .3s ease-in;
+            margin: 0.2em;
+        }
+        .timerLinks a.active {
+            opacity: 1;
+        }
+    </style>
+</head>
+<body>
+    <h1>''' + args.log_name + '''</h1>
+    <div class="log">
+'''
+
+    for (timestamp, username, message) in line_generator:
+        yield '''        <div class="line">
+            <span class="timestamp">'''
+        yield timestamp
+        yield '''</span>
+            <span class="username">'''
+        if username == "*":
+            yield '''</span>
+            <span class="message"><strong>'''
+            yield message.split()[0]
+            yield '''</strong> '''
+            yield " ".join(message.split()[1:])
+        else:
+            yield '''&lt;'''
+            yield username
+            yield '''&gt;</span>
+            <span class="message">'''
+            yield message
+        yield '''</span>
+        </div>
+'''
+
+    yield '''    </div>'''
+    if args.add_timer:
+        yield '''    <script type="text/javascript">
+        var timerLinks = document.createElement("div");
+        var timedLink = document.createElement("a");
+        var untimedLink = document.createElement("a");
+        timedLink.href = "#timed";
+        untimedLink.href = "#";
+        timerLinks.textContent = "Timer:"
+        timedLink.textContent = "On"
+        untimedLink.textContent = "Off"
+        timerLinks.classList.add("timerLinks");
+        timerLinks.appendChild(timedLink);
+        timerLinks.appendChild(untimedLink);
+        document.body.appendChild(timerLinks);
+
+        var lastTimer = null;
+        var lines = document.getElementsByClassName("line");
+
+        var parseDate = function(dateString) {
+            var dateRegex = /([0-9]+)-([0-9]+)-([0-9]+) ([0-9]{2}):([0-9]{2}):([0-9]{2})/;
+            dateMatch = dateRegex.exec(dateString);
+            if(dateMatch == null) {
+                return null;
+            } else {
+                return new Date(
+                        parseInt(dateMatch[1]),
+                        parseInt(dateMatch[2]),
+                        parseInt(dateMatch[3]),
+                        parseInt(dateMatch[4]),
+                        parseInt(dateMatch[5]),
+                        parseInt(dateMatch[6]));
+            }
+        }
+
+        var initFunction = function() {
+            if(lastTimer != null) {
+                clearTimeout(lastTimer);
+            }
+
+            if(location.hash == "#timed") {
+                timedLink.classList.add("active");
+                untimedLink.classList.remove("active");
+
+                for(var i = 0; i < lines.length; i++) {
+                    lines[i].classList.add("timeHidden");
+                }
+
+                var showNextLine = function() {
+                    lines[this].classList.remove("timeHidden");
+                    if(this < lines.length) {
+                        setTimeout(showNextLine.bind(this+1), parseDate(lines[this+1].getElementsByClassName("timestamp")[0].textContent) - parseDate(lines[this].getElementsByClassName("timestamp")[0].textContent));
+                    }
+                };
+
+                showNextLine.bind(0)();
+            } else {
+                untimedLink.classList.add("active");
+                timedLink.classList.remove("active");
+
+                for(var i = 0; i < lines.length; i++) {
+                    lines[i].classList.remove("timeHidden");
+                }
+            }
+        };
+
+        window.addEventListener("hashchange", initFunction);
+        initFunction();
+    </script>
+'''
+    yield '''</body>
+</html>'''
+
+
 @renderer_function("plaintext", "emits reflowed plaintext",
         (('-1', '--single-pass', {
             'action': 'store_true',
