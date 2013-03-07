bc81f72e9fc7138a0c7901624882d61337d27220
Psychedelic Squid <psquid@psquid.net>
2013-03-07 14:12:54 +0000

    --ignore-{type} arguments for dropping various message types.

--- prettify/prettify/app.py
+++ prettify/prettify/app.py
@@ -10,11 +10,19 @@ def register_reader(reader_fn, reader_name, reader_desc="", reader_options=()):
 def register_renderer(renderer_fn, renderer_name, renderer_desc="", renderer_options=()):
     renderers[renderer_name] = (renderer_fn, renderer_desc, renderer_options)
 
-def prettify_log(ugly_log, reader, reader_args, renderer, renderer_args):
-    return renderer(reader(ugly_log, reader_args), renderer_args)
+def prettify_log(ugly_log, reader, reader_args, renderer, renderer_args,
+        ignore_types):
+    if ignore_types is not None:
+        def wrapped_reader():
+            for line in reader(ugly_log, reader_args):
+                if not type(line[1]) in ignore_types:
+                    yield line
+        return renderer(wrapped_reader(), renderer_args)
+    else:
+        return renderer(reader(ugly_log, reader_args), renderer_args)
 
 def run():
-    import argparse, sys, prettify.reader, prettify.renderer
+    import argparse, sys, prettify.reader, prettify.renderer, prettify.messages
 
     parser = argparse.ArgumentParser(description='''Prettify an IRC log.''')
     parser.add_argument('-r', '--reader', action='store', default='weechat',
@@ -34,6 +42,27 @@ def run():
             default=sys.stdout, type=argparse.FileType('w'), help='''Output file
             to write to (STDOUT will be used if this is not given).''',
             dest='outfile')
+    parser.add_argument('--ignore-privmsg', action='append_const',
+            const=prettify.messages.PrivMsg, dest="ignore_types",
+            help='''Drop PRIVMSG lines (regular chat and PMs).''')
+    parser.add_argument('--ignore-action', action='append_const',
+            const=prettify.messages.Action, dest="ignore_types",
+            help='''Drop action lines (/me actions).''')
+    parser.add_argument('--ignore-notice', action='append_const',
+            const=prettify.messages.Notice, dest="ignore_types",
+            help='''Drop notice lines.''')
+    parser.add_argument('--ignore-join', action='append_const',
+            const=prettify.messages.Join, dest="ignore_types",
+            help='''Drop join lines.''')
+    parser.add_argument('--ignore-part', action='append_const',
+            const=prettify.messages.Part, dest="ignore_types",
+            help='''Drop part lines.''')
+    parser.add_argument('--ignore-quit', action='append_const',
+            const=prettify.messages.Quit, dest="ignore_types",
+            help='''Drop quit lines.''')
+    parser.add_argument('--ignore-system', action='append_const',
+            const=prettify.messages.System, dest="ignore_types",
+            help='''Drop system message lines.''')
     args, unknown_args = parser.parse_known_args()
 
     reader_parser = argparse.ArgumentParser(description=readers[args.reader][1])
@@ -55,5 +84,5 @@ def run():
     if not (args.reader_help or args.renderer_help):
         for line in prettify_log(args.infile,
                 readers[args.reader][0], reader_args,
-                renderers[args.renderer][0], renderer_args):
+                renderers[args.renderer][0], renderer_args, args.ignore_types):
             args.outfile.write(line)
