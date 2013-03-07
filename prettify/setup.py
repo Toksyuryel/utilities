0c0c085ea886381e035419d502fbe6d350a6e70a
Psychedelic Squid <psquid@psquid.net>
2013-03-07 14:52:33 +0000

    That is not how you classify.

--- prettify/setup.py
+++ prettify/setup.py
@@ -18,7 +18,7 @@ setup(
 
         classifiers = [
             "Programming Language :: Python",
-            "Programming Language :: Python 3",
+            "Programming Language :: Python :: 3",
             "License :: OSI Approved :: BSD License",
             "Operating System :: OS Independent",
             "Development Status :: 4 - Beta",
