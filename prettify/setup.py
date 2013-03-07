dc550e47d76123e83f8defb35799b09804c28c4f
Psychedelic Squid <psquid@psquid.net>
2013-03-07 15:03:46 +0000

    Bump version.

--- prettify/setup.py
+++ prettify/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "prettify",
-        version = "0.1.0",
+        version = "0.1.1",
         description = "IRC log prettifier",
         url = "https://pypi.python.org/pypi/prettify",
 
