238d95cfbca0ec2fdb790c4e4d589abda61b0489
Bit Shift <bitshift@bigmacintosh.net>
2013-07-22 16:36:06 +0100

    [I.sh] Fix 'finished' USAGE line, + new flag/alias

--- I.sh
+++ I.sh
@@ -20,8 +20,9 @@ usage() {
         echo "A time tracker for the command line."
         echo
         echo "  started ACTIVITY      Make an entry for starting ACTIVITY."
-        echo "  finished REGEX        Make an entry for finishing ongoing activity number NUM."
-        echo "  did                   Show completed activities."
+        echo "  finished NUM          Make an entry for finishing ongoing activity number NUM."
+        echo "  stopped NUM           Alias for 'finished NUM'"
+        echo "  did [-g|--graphical]  Show completed activities (optionally as a graph)."
         echo "  am                    Show ongoing activities."
         echo
         echo "Activities are stored in the file named in \$I_ACTIVITY_FILE, which defaults to \$HOME/.Iactivities. The file will be created if nonexistent."
