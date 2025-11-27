6371f3d3179562cc1b947eb37129655f006fc006
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2025-11-26 22:30:35 -0800

    Initial attempt at a verbose mode, very rough still.

--- mvln.sh
+++ mvln.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# set -x
+
 usage() {
     printf 'USAGE: %s SOURCE... DIRECTORY\n' "$(basename "$0")" 1>&2
     printf 'Move SOURCE(s) to DIRECTORY, leaving behind symbolic links.\n' 1>&2
@@ -31,11 +33,23 @@ normalize () {
 
 command_gen() {
     cat << %
-mv "$1" "$2" && ln -s "$(getfullpath "$2")/$(basename "$1")" "$1"
+mv "$1" "$2" && ln -s "$3" "$1"
 %
 }
 
 depend sed awk
+
+unset OPTV
+
+while getopts v OPT
+do
+    case $OPT in
+    v) OPTV=1;;
+    ?) usage;;
+    esac
+done
+
+shift $((OPTIND - 1))
 
 [ $# -ge 2 ] || usage
 DST="$(getlast "$@")"
@@ -45,6 +59,11 @@ DST="$(getlast "$@")"
 while [ $# -gt 1 ]
 do
     [ -e "$1" ] || usage
-    $CMD "$(command_gen "$(normalize "$1")" "$DST")"
+    TGT="$(getfullpath "$2")/$(basename "$1")"
+    $CMD "$(command_gen "$(normalize "$1")" "$DST" "$TGT")"
+    if [ -z "$DEBUG" ] && [ ! -z "$OPTV" ]; then
+        printf '%s\n' "mv: renamed '$1' -> '$DST/$(normalize "$1")'"
+        printf '%s\n' "ln: '$1' -> '$TGT'"
+    fi
     shift
 done
