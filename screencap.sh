ac9d46c4fa1f519760e4b48111be72cc2b7a3a41
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2025-12-07 17:01:22 -0800

    screencap.sh: minor code cleanup

--- screencap.sh
+++ screencap.sh
@@ -30,7 +30,7 @@ checkdir() {
     if [ -e "$DIR" ]; then
       [ -d "$DIR" ] || die "$DIR exists but is not a directory."
       [ -x "$DIR" ] && [ -w "$DIR" ] || die "$DIR exists but is not writable."
-      else
+    else
       # shellcheck disable=SC2174
       mkdir -p -m 0700 "$DIR" || die "$DIR doesn't exist and could not be created."
     fi
@@ -39,15 +39,15 @@ checkdir() {
 
 checkfmt() {
   magick identify -list format \
-  | grep -iE '^[[:space:]]*'"$1"'(\*)?[[:space:]]+' \
-  | awk '{ print $3 }' \
-  | grep -q 'w'
+    | grep -iE '^[[:space:]]*'"$1"'(\*)?[[:space:]]+' \
+    | awk '{ print $3 }' \
+    | grep -q 'w'
 }
 
 capture() {
   if [ "$1" = "root" ]; then
     shift && set -- -window root "$@"
-    else
+  else
     shift
   fi
   magick import -silent "$@"
@@ -58,13 +58,12 @@ xset q > /dev/null 2>&1 || die "Can't find X session."
 
 unset MODE CAPTURE_DIR CAPTURE_FMT
 
-while getopts d:f:r OPT
-do
+while getopts d:f:r OPT; do
   case $OPT in
-  d)  CAPTURE_DIR="$OPTARG";;
-  f)  CAPTURE_FMT="$OPTARG";;
-  r)  MODE="root";;
-  ?)  usage;;
+    d)  CAPTURE_DIR="$OPTARG" ;;
+    f)  CAPTURE_FMT="$OPTARG" ;;
+    r)  CAPTURE_MODE="root" ;;
+    ?)  usage ;;
   esac
 done
 
@@ -72,16 +71,18 @@ shift $((OPTIND - 1))
 
 [ "$CAPTURE_FMT" ] || CAPTURE_FMT="png"
 checkfmt "$CAPTURE_FMT" || die "output format '$CAPTURE_FMT' is not supported on your system."
+
 [ "$CAPTURE_DIR" ] || CAPTURE_DIR="${XDG_PICTURES_DIR:-"${HOME}/Pictures"}"
 CAPTURE_TMPDIR="${XDG_CACHE_HOME:-"${HOME}/.cache"}"
 checkdir "$CAPTURE_DIR" "$CAPTURE_TMPDIR"
-CAPTURE_TMP="${CAPTURE_TMPDIR}/screencap_tmp.${CAPTURE_FMT}"
 
-capture "$MODE" "$CAPTURE_TMP"
+CAPTURE_TMP="${CAPTURE_TMPDIR}/screencap_tmp.${CAPTURE_FMT}"
+capture "$CAPTURE_MODE" "$CAPTURE_TMP"
 
 CAPTURE_TIME="$(date +%F-%H%M%S)"
 CAPTURE_DIMS="$(magick identify -format '%wx%h' "$CAPTURE_TMP")"
 CAPTURE_SUFX="screencap"
+
 CAPTURE_PATH="${CAPTURE_DIR}/${CAPTURE_TIME}_${CAPTURE_DIMS}_${CAPTURE_SUFX}.${CAPTURE_FMT}"
 mv "$CAPTURE_TMP" "$CAPTURE_PATH"
 type notify-send > /dev/null 2>&1 && notify-send -a "$0" "Screenshot saved" "$CAPTURE_PATH"
