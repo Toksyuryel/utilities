f4f7506ee62e8a2d4643d12cfc864f07b97eb3e2
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2025-12-15 05:47:23 -0800

    mkstemps.sh: remove unused return values

--- mkstemps.sh
+++ mkstemps.sh
@@ -4,28 +4,32 @@
 # a reimplementation of mkstemps(3) for use in POSIX-compatible shell scripts
 # needed because mktemp is not in POSIX and m4 only provides mkstemp
 mkstemps() (
-  [ $# -gt 0 ] && [ $# -lt 3 ] || return 1
-
-  if [ $# -eq 1 ]; then
-    suffix="$1"
-  else
-    suffix="$2"
-    if [ ! -d "$1" ]; then
-      printf '%s' "$1" | grep "^[^X]*XXXXXX$" > /dev/null 2>&1 || return 1
-      [ -x "$(dirname "$1")" ] && [ -w "$(dirname "$1")" ] || return 1
-      template="$1"
+  unset template suffix
+  if [ $# -gt 0 ] && [ $# -lt 3 ]; then
+    if [ $# -eq 1 ]; then
+      suffix="$1"
     else
-      [ -x "$1" ] && [ -w "$1" ] || return 1
-      template="${1}/"
+      suffix="$2"
+      if [ -d "$1" ]; then
+        [ -x "$1" ] && [ -w "$1" ] && template="${1}/"
+      else
+        tmpdir="$(dirname "$1")"
+        if printf '%s' "$1" | grep "^[^X]*XXXXXX$" > /dev/null 2>&1 \
+          && [ -x "$tmpdir" ] && [ -w "$tmpdir" ]; then
+          template="$1"
+        fi
+      fi
     fi
   fi
-
-  while
-    tempfile="$(printf 'mkstemp(%s)' "$template" | m4)"
-    target="$(printf '%s%s' "$tempfile" "$suffix")"
-    link "$tempfile" "$target"; unlink "$tempfile"
-    [ ! -e "$target" ]
-  do : ; done
-  [ -e "$target" ] || return 2
-  printf '%s' "$target"
+  if [ "$suffix" ]; then
+    if [ $# -eq 1 ] || [ "$template" ]; then
+      while
+        tempfile="$(printf 'mkstemp(%s)' "$template" | m4)"
+        target="$(printf '%s%s' "$tempfile" "$suffix")"
+        link "$tempfile" "$target"; unlink "$tempfile"
+        [ ! -e "$target" ]
+      do : ; done
+      printf '%s' "$target"
+    fi
+  fi
 )
