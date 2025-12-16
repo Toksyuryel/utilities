a22ab0e629c636615a9d99a634325a4670e95d5f
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2025-12-15 16:43:53 -0800

    Revert "mkstemps.sh: remove unused return values"
    This reverts commit f4f7506ee62e8a2d4643d12cfc864f07b97eb3e2.

--- mkstemps.sh
+++ mkstemps.sh
@@ -4,32 +4,28 @@
 # a reimplementation of mkstemps(3) for use in POSIX-compatible shell scripts
 # needed because mktemp is not in POSIX and m4 only provides mkstemp
 mkstemps() (
-  unset template suffix
-  if [ $# -gt 0 ] && [ $# -lt 3 ]; then
-    if [ $# -eq 1 ]; then
-      suffix="$1"
+  [ $# -gt 0 ] && [ $# -lt 3 ] || return 1
+
+  if [ $# -eq 1 ]; then
+    suffix="$1"
+  else
+    suffix="$2"
+    if [ ! -d "$1" ]; then
+      printf '%s' "$1" | grep "^[^X]*XXXXXX$" > /dev/null 2>&1 || return 1
+      [ -x "$(dirname "$1")" ] && [ -w "$(dirname "$1")" ] || return 1
+      template="$1"
     else
-      suffix="$2"
-      if [ -d "$1" ]; then
-        [ -x "$1" ] && [ -w "$1" ] && template="${1}/"
-      else
-        tmpdir="$(dirname "$1")"
-        if printf '%s' "$1" | grep "^[^X]*XXXXXX$" > /dev/null 2>&1 \
-          && [ -x "$tmpdir" ] && [ -w "$tmpdir" ]; then
-          template="$1"
-        fi
-      fi
+      [ -x "$1" ] && [ -w "$1" ] || return 1
+      template="${1}/"
     fi
   fi
-  if [ "$suffix" ]; then
-    if [ $# -eq 1 ] || [ "$template" ]; then
-      while
-        tempfile="$(printf 'mkstemp(%s)' "$template" | m4)"
-        target="$(printf '%s%s' "$tempfile" "$suffix")"
-        link "$tempfile" "$target"; unlink "$tempfile"
-        [ ! -e "$target" ]
-      do : ; done
-      printf '%s' "$target"
-    fi
-  fi
+
+  while
+    tempfile="$(printf 'mkstemp(%s)' "$template" | m4)"
+    target="$(printf '%s%s' "$tempfile" "$suffix")"
+    link "$tempfile" "$target"; unlink "$tempfile"
+    [ ! -e "$target" ]
+  do : ; done
+  [ -e "$target" ] || return 2
+  printf '%s' "$target"
 )
